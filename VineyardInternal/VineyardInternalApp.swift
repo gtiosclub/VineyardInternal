694a0a0f318643aa07bda1aa38316ec858576e80
vishneshjr <vishnesh@gatech.edu>
2024-09-11 17:16:03 -0400

    Initial Commit

---
+++ VineyardInternal/VineyardInternalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VineyardInternalApp.swift
+//  VineyardInternal
+//
+//  Created by Vishnesh Jayanthi Ramanathan on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct VineyardInternalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
